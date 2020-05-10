990cdb8eccf93eb6581502e6b5491c97cbcd08ab
Jed <jedhcl@gmail.com>
2020-05-09 23:26:22 -0700

    Use TLS

--- config.py
+++ config.py
@@ -10,8 +10,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAILGUN_SMTP_SERVER')
     MAIL_PORT = 587
-    MAIL_USE_SSL = True
-    MAIL_USE_TLS = False
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAILGUN_SMTP_LOGIN')
     MAIL_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD')
     MAILGUN_DOMAIN = os.environ.get('MAILGUN_DOMAIN')
